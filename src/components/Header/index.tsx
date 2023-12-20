8c07a49de01e18c74a5f9147887c8517184a4170
pedrollcopatti <pedrolucascopatti@gmail.com>
2023-12-20 14:49:47 -0300

    meta tags and changes

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,39 +1,61 @@
 import { Button, Flex, HStack } from "@chakra-ui/react";
 
-import logo from '@assets/logo.png';
+import logo from "@assets/logo.svg";
 import Image from "next/image";
 import { Link } from "react-scroll";
 
-export default function Header(){
-    return(
-        <HStack zIndex={999} bgColor={'black.500'} top={0} position={'sticky'} justifyContent={'space-between'} paddingX={20} width={'100%'} height={24}>
-        <Flex width={16} >
-          <Image alt="logo" src={logo}/>
-        </Flex>
-        <HStack>
-          <Link to="entrega" smooth offset={-24}>
-          <Button fontSize={'sm'}variant={'ghost'}>
+export default function Header() {
+  return (
+    <HStack
+      zIndex={999}
+      bgColor={"black.500"}
+      top={0}
+      position={"sticky"}
+      justifyContent={"space-between"}
+      paddingX={24}
+      width={"100%"}
+      height={24}
+    >
+      <Flex width={36}>
+        <Image alt="logo" src={logo} />
+      </Flex>
+      <HStack>
+        <Link to="entrega" smooth offset={-24}>
+          <Button
+            _hover={{ backgroundColor: "black.50" }}
+            color={"white"}
+            fontSize={"sm"}
+            variant={"ghost"}
+          >
             O QUE ENTREGAMOS
-          </Button></Link>
-          <Link to="thrive" smooth offset={-150}>
-          <Button fontSize={'sm'}variant={'ghost'}>
+          </Button>
+        </Link>
+        <Link to="thrive" smooth offset={-150}>
+          <Button
+            _hover={{ backgroundColor: "black.50" }}
+            color={"white"}
+            fontSize={"sm"}
+            variant={"ghost"}
+          >
             A THRIVE
           </Button>
-          </Link>
-          <Button
-            _hover={{
-              background: 'orange.500'
-            }}
-            borderRadius={8}
-            colorScheme="orange"
-            background={'linear-gradient(92deg, #D94B00 0%, #FF5800 50.52%, #BE4200 100%)'}
-            color={"white.500"}
-            backgroundColor={'orange.500'}
-            fontWeight={'bold'}
-          >
-            FALAR COM A THRIVE
-          </Button>
-        </HStack>
+        </Link>
+        <Button
+          _hover={{
+            background: "orange.500",
+          }}
+          borderRadius={8}
+          colorScheme="orange"
+          background={
+            "linear-gradient(92deg, #D94B00 0%, #FF5800 50.52%, #BE4200 100%)"
+          }
+          color={"white.500"}
+          backgroundColor={"orange.500"}
+          fontWeight={"bold"}
+        >
+          FALAR COM A THRIVE
+        </Button>
+      </HStack>
     </HStack>
-    )
+  );
 }
--- src/components/Texture/index.tsx
+++ src/components/Texture/index.tsx
@@ -9,7 +9,7 @@ export default function Texture() {
       id="noise"
       width={"100%"}
       height={"100%"}
-      opacity={0.5}
+      opacity={0.7}
     />
   );
 }
